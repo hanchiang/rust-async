179bf082c5b376e073639802d3bbd56c96774d48
han chiang <yaphc@hotmail.com>
2022-08-26 23:57:35 +0800

    Pinning

---
+++ 4 - pinning/src/main.rs
@@ -0,0 +1,66 @@
+mod pinning_to_stack;
+mod pinning_to_heap;
+
+#[derive(Debug)]
+struct Test {
+    a: String,
+    b: *const String,
+}
+
+impl Test {
+    fn new(txt: &str) -> Self {
+        Test {
+            a: String::from(txt),
+            b: std::ptr::null(),
+        }
+    }
+
+    fn init(&mut self) {
+        let self_ref: *const String = &self.a;
+        self.b = self_ref;
+    }
+
+    fn a(&self) -> &str {
+        &self.a
+    }
+
+    fn b(&self) -> &String {
+        assert!(!self.b.is_null(), "Test::b called without Test::init being called first");
+        unsafe { &*(self.b) }
+    }
+}
+
+fn basic() {
+    let mut test1 = Test::new("test1");
+    test1.init();
+    let mut test2 = Test::new("test2");
+    test2.init();
+
+    println!("a: {}, b: {}", test1.a(), test1.b());
+    println!("a: {}, b: {}", test2.a(), test2.b());
+    // a: test1, b: test1
+    // a: test2, b: test2
+}
+
+fn swap() {
+    let mut test1 = Test::new("test1");
+    test1.init();
+    let mut test2 = Test::new("test2");
+    test2.init();
+
+    println!("a: {}, b: {}", test1.a(), test1.b());
+    std::mem::swap(&mut test1, &mut test2);
+    println!("a: {}, b: {}", test2.a(), test2.b());
+
+    // a: test1, b: test1
+    // a: test1, b: test2
+}
+
+fn main() {
+    println!("pinning to basic");
+    basic();
+    swap();
+
+    pinning_to_stack::main();
+    pinning_to_heap::main();
+}
---
+++ 4 - pinning/src/pinning_to_heap.rs
@@ -0,0 +1,44 @@
+use std::marker::PhantomPinned;
+use std::pin::Pin;
+
+#[derive(Debug)]
+struct Test {
+    a: String,
+    b: *const String,
+    _marker: PhantomPinned,
+}
+
+impl Test {
+    fn new(txt: &str) -> Pin<Box<Self>> {
+        let t = Test {
+            a: String::from(txt),
+            b: std::ptr::null(),
+            _marker: PhantomPinned,
+        };
+        // Pinning an !Unpin type to the heap gives our data a stable address
+        // so we know that the data we point to can't move after it's pinned.
+        // In contrast to stack pinning, we know that the data will be pinned for the lifetime of the object.
+        let mut boxed = Box::pin(t);
+        let self_ptr: *const String = &boxed.a;
+        unsafe { boxed.as_mut().get_unchecked_mut().b = self_ptr };
+
+        boxed
+    }
+
+    fn a(self: Pin<&Self>) -> &str {
+        &self.get_ref().a
+    }
+
+    fn b(self: Pin<&Self>) -> &String {
+        unsafe { &*(self.b) }
+    }
+}
+
+pub fn main() {
+    let test1 = Test::new("test1");
+    let test2 = Test::new("test2");
+
+    println!("pinning to heap");
+    println!("a: {}, b: {}",test1.as_ref().a(), test1.as_ref().b());
+    println!("a: {}, b: {}",test2.as_ref().a(), test2.as_ref().b());
+}
